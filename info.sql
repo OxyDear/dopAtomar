ed6dd8353f1de915873ed6b3c7cb716389c8f810
Ivan <102287400+OxyDear@users.noreply.github.com>
2025-02-19 11:31:07 +0300

    Update info.sql

--- info.sql
+++ info.sql
@@ -1,1 +1,1 @@
-SELECT GroupName, FullName, discipline, unnest(string_to_array(mark, ' '))::TEXT FROM students;
+SELECT * FROM (SELECT GroupName, FullName, discipline, unnest(string_to_array(mark, ' '))::TEXT AS marks FROM students) WHERE marks != ''; 
